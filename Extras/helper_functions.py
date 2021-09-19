376a0a8adf93f1a6ef09a3a65ec23daca153906b
Daksh patel <69884797+helloitsdaksh@users.noreply.github.com>
2021-09-20 02:51:39 +0530

    Update helper_functions.py

--- Extras/helper_functions.py
+++ Extras/helper_functions.py
@@ -31,7 +31,7 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 
 # Our function needs a different name to sklearn's plot_confusion_matrix
-ef make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
   """Makes a labelled confusion matrix comparing predictions and ground truth labels.
 
   If classes is passed, confusion matrix will be labelled, if not, integer class values
